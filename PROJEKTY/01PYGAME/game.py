c8a133605a0585ac310ae80594df2ede941b63e0
thuy <nguyen122@trebesin.cz>
2025-01-31 19:49:23 +0100

    sobota

--- PROJEKTY/01PYGAME/game.py
+++ PROJEKTY/01PYGAME/game.py
@@ -13,8 +13,11 @@ clock = pygame.time.Clock()
 
 running = True #nekonecný cyklus 
 
+player_path = (r"C:\Users\nqthu\Downloads\PYTHON (1)\game.penguin\PROJEKTY\01PYGAME\penguin.png")
+enemy_path = (r"C:\Users\nqthu\Downloads\PYTHON (1)\game.penguin\PROJEKTY\01PYGAME\fox.png")
+
 # player = pygame.Rect((50, 100, 50, 50))#vytvoření hráče -> hráč je obdélník x,y,šířka,výška , dvojite zavorky protoze jsme vlozili jednodušší a rychlejší list
-player_surf = pygame.image.load("penguin.png").convert_alpha()#aplha pruhledny pozadí
+player_surf = pygame.image.load(player_path).convert_alpha()#aplha pruhledny pozadí
 player_surf = pygame.transform.rotozoom(player_surf, 0, 2)
 player_x = 200
 player_y = 150
@@ -22,12 +25,23 @@ player_rect = player_surf.get_rect(midbottom=(player_x, player_y))
 player_speed = 5
 player_lives = 3
 
-enemy_surf = pygame.image.load("fox.png").convert_alpha()
+enemy_surf = pygame.image.load(enemy_path).convert_alpha()
 enemy_surf = pygame.transform.rotozoom(enemy_surf,0, 2)#koho, rotovani , velikost-scale
 enemy_x = screen_width - 20
 enemy_y = 300
 enemy_speed = 5
 enemy_rect = enemy_surf.get_rect(midbottom=(enemy_x, enemy_y))
+
+def penguin_move(key ,player_rect):
+    if key[pygame.K_w]:
+        player_rect.top -= player_speed
+    if key[pygame.K_a]:
+        player_rect.left -= player_speed
+    if key[pygame.K_s]:
+        player_rect.bottom += player_speed
+    if key[pygame.K_d]:
+        player_rect.right += player_speed
+
 
 while running:
 
@@ -37,6 +51,8 @@ while running:
             exit()
     
     key = pygame.key.get_pressed()
+    penguin_move(key, player_rect)
+
 
     # if key[pygame.K_RIGHT]:
     #     player.move_ip(2, 0)#pohybame player díky funkci move_ip a v zavorce o kolik pohybuje x,y
@@ -47,14 +63,7 @@ while running:
     # if key[pygame.K_DOWN]:
     #     player.move_ip(0, 2)
 
-    if key[pygame.K_w]:
-        player_rect.top -= player_speed
-    if key[pygame.K_a]:
-        player_rect.left -= player_speed
-    if key[pygame.K_s]:
-        player_rect.bottom += player_speed
-    if key[pygame.K_d]:
-        player_rect.right += player_speed
+
   
     screen.fill("powderblue")
 
--- PROJEKTY/03CANDY/candy.py
+++ PROJEKTY/03CANDY/candy.py
@@ -1,34 +1,110 @@
 import pygame
 import os
 
-pygame.display.set_caption("první hra")
+print(os.getcwd())
 
-assets_path = "/game.penguin/PROJEKTY/ASSETS/jelly_left.png"
+pygame.display.set_caption("první hra")
 
 WIDTH, HEIGHT = 900, 500 #velikost obrazovky
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 CREAM_COL = (242, 219, 163)
+BLACK = (0, 0, 0)
+PINK = (232, 0, 156)
+YELLOW = (232, 224, 10)
+
+BORDER = pygame.Rect(WIDTH//2 - 7.5 , 0, 15, HEIGHT)
+
+# HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
+# WINNER_FONT = pygame.font.SysFont('comicsans', 100)
+
 FPS = 60 #kolik framu za sekundu
+MOVEMENT = 5
+
+BUL_WIDTH = 10
+BUL_HEIGHT = 5
+BULLET_MOVEMENT = 7
+MAX_BULLET = 3
+
+YELLOW_HIT = pygame.USEREVENT + 1 #1==unique id eventu,yellow a pink nemuzou mit stejne cislo
+PINK_HIT = pygame.USEREVENT + 2 #nejakej event
+
+#r ->rawstring dělá, aby to bral "\" jako obyč znak(v pythonu je "\" escape backlash ) 
+#nebo můžeme "\" zdvojit viz. \\
+#nebo místo "\" použít "/"
+JELLY_PATH = (r"C:\Users\nqthu\Downloads\PYTHON (1)\game.penguin\PROJEKTY\03CANDY\jelly_left.png")
+TEDDY_PATH = ("C:/Users/nqthu/Downloads/PYTHON (1)/game.penguin/PROJEKTY/03CANDY/teddy_right.png")
+
+#u teddy jsem použila rotate u jelly ne
 JELLY_WIDTH, JELLY_HEIGHT = 80, 80
 TEDDY_WIDTH, TEDDY_HEIGHT = 80, 80
+TEDDY = pygame.image.load(TEDDY_PATH)
+TEDDY = pygame.transform.rotate(pygame.transform.scale(TEDDY, (TEDDY_WIDTH, TEDDY_HEIGHT)), -90)#90stupnu rotace
+JELLY = pygame.image.load(JELLY_PATH)
+JELLY = pygame.transform.scale(JELLY, (JELLY_WIDTH, JELLY_HEIGHT))#nerotace:D
 
 
-JELLY = pygame.image.load(os.path.join(assets_path))
-JELLY = pygame.transform.scale(JELLY, (JELLY_WIDTH, JELLY_HEIGHT))
-TEDDY = pygame.image.load("teddy_right.pgn")
-TEDDY = pygame.transform.scale(TEDDY, (TEDDY_WIDTH, TEDDY_HEIGHT))
+def yellow_move(keys_pressed, yellow):
+        if keys_pressed[pygame.K_a] and yellow.x - MOVEMENT + 10 > 0:
+             yellow.x -= MOVEMENT
+        if keys_pressed[pygame.K_d] and yellow.x + MOVEMENT + TEDDY_WIDTH - 6 < BORDER.x : #musí tam být teddy_width, aby to nepřekročilo border
+             yellow.x += MOVEMENT
+        if keys_pressed[pygame.K_w] and yellow.y - MOVEMENT + 10 > 0:
+             yellow.y -= MOVEMENT
+        if keys_pressed[pygame.K_s] and yellow.y + MOVEMENT + TEDDY_HEIGHT - 5 < HEIGHT:
+             yellow.y += MOVEMENT
+
+def pink_move(keys_pressed, pink):
+        if keys_pressed[pygame.K_LEFT] and pink.x - MOVEMENT - 5 > BORDER.x :#left
+             pink.x -= MOVEMENT
+        if keys_pressed[pygame.K_RIGHT] and pink.x + JELLY_WIDTH + MOVEMENT - 10 < WIDTH:#right -10je par pixelů, aby to bylo víc clean ale není to potřebný
+             pink.x += MOVEMENT
+        if keys_pressed[pygame.K_UP] and pink.y - MOVEMENT + 12 > 0: #up
+             pink.y -= MOVEMENT
+        if keys_pressed[pygame.K_DOWN] and pink.y + MOVEMENT + JELLY_HEIGHT - 15 < HEIGHT :#down
+             pink.y += MOVEMENT
+
+def handle_bullets(yellow_bullets, pink_bullets, yellow, pink):
+     for bullet in yellow_bullets:
+          bullet.x += BULLET_MOVEMENT
+          if pink.colliderect(bullet):#pokud pink se setká s bullet
+               pygame.event.post(pygame.event.Event(PINK_HIT))
+               yellow_bullets.remove(bullet)
+          elif bullet.x > WIDTH:
+               yellow_bullets.remove(bullet)
 
 
+     for bullet in pink_bullets:
+          bullet.x -= BULLET_MOVEMENT
+          if yellow.colliderect(bullet):#pokud pink se setká s bullet
+               pygame.event.post(pygame.event.Event(YELLOW_HIT))
+               pink_bullets.remove(bullet)    
+          elif bullet.x < 0:
+               pink_bullets.remove(bullet) 
+#díky tomuhle je to actually vidět
 
-def grafics():
+def grafics(pink, yellow, pink_bullets, yellow_bullets ):#aby funkce znala proměné pink yellow
         SCREEN.fill(CREAM_COL)#barva pozadí
-        SCREEN.blit(JELLY, (750, 250))#vyblít na screen
-        SCREEN.blit(TEDDY, (150, 250))
+        pygame.draw.rect(SCREEN, BLACK, BORDER)#kam kreslíme, barvu a co kreslíme
+        SCREEN.blit(JELLY, (pink.x, pink.y))#jelly zastupuje pink rectangle->proto x==700 y==300
+        SCREEN.blit(TEDDY, (yellow.x, yellow.y))
+
+        for bullet in pink_bullets:
+             pygame.draw.rect(SCREEN, PINK, bullet)
+        for bullet in yellow_bullets:
+             pygame.draw.rect(SCREEN, YELLOW, bullet)
+
         pygame.display.update()#musime updatenout, aby se to cream col ukázala
 
 
 
+
 def main():
+    pink = pygame.Rect(700, 300, JELLY_WIDTH, JELLY_HEIGHT)#
+    yellow = pygame.Rect(100, 300, TEDDY_WIDTH, TEDDY_HEIGHT)#pink.x position, y position, width, height
+
+    pink_bullets = []
+    yellow_bullets = []
+
     clock = pygame.time.Clock()
     
     running = True #nekonečný cyklus
@@ -37,8 +113,25 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT: #pokud se stane event quit ukonči hru
                 running = False
+
+            if event.type == pygame.KEYDOWN:
+                 if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLET:#leftcontol
+                      bullet = pygame.Rect(
+                           yellow.x + TEDDY_WIDTH - 6, yellow.y + TEDDY_HEIGHT//2 - 2.5, BUL_WIDTH,BUL_HEIGHT)#/2 vystřelí zprostředka výšky teddy,-2.5->pol výškybullet,
+                      yellow_bullets.append(bullet)    
+
+                 if event.key == pygame.K_RCTRL and len(pink_bullets) < MAX_BULLET:#nestrilej dalsi bullets pokud na screen už je maximalni pocet bullets
+                      bullet = pygame.Rect(
+                           pink.x, pink.y + JELLY_HEIGHT//2 , BUL_WIDTH, BUL_HEIGHT)
+                      pink_bullets.append(bullet)
+          
+        keys_pressed = pygame.key.get_pressed()
+        yellow_move(keys_pressed, yellow)
+        pink_move(keys_pressed, pink)
         
-        grafics()
+        handle_bullets(yellow_bullets, pink_bullets, yellow, pink)
+    
+        grafics(pink, yellow, pink_bullets, yellow_bullets)
     
     pygame.quit()
 
