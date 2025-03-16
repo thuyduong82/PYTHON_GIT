08bd498005a507b6e77c5af6f471e95294e8cffa
thuy <nguyen122@trebesin.cz>
2025-03-16 22:03:45 +0100

    pokus o snake

---
+++ PROJEKTY/04SNAKE/snake.py
@@ -0,0 +1,44 @@
+import pygame
+
+# pygame setup
+pygame.init()
+WIDTH, HEIGHT = 900, 500 #velikost obrazovky
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(SCREEN.get_width() / 2, SCREEN.get_height() / 2)
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    SCREEN.fill("purple")
+
+    pygame.draw.circle(SCREEN, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
