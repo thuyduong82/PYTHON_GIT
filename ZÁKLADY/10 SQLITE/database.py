52d08025024a431dc62765c8ee1306a5360de103
Thuy Duong <nguyen122@trebesin.cz>
2025-10-08 10:44:24 +0200

    databaze

---
+++ ZÁKLADY/10 SQLITE/database.py
@@ -0,0 +1,22 @@
+import sqlite3
+#vytvoř spojení s databází
+
+
+#vytvor spojeni s databazi
+connection = sqlite3.connect("game.db")
+
+#vytvoreni kurzoru, ktery nam umoznuje navigaco po databazi
+cursor = connection.cursor
+
+#zapis do db
+
+user_input = input("přidej postavičku do databáze:")
+
+cursor.execute(f"""
+        INSERT INTO characters (name) VALUES ({user_input})
+               """)
+
+#potvrzeni vlozeni dat do db
+connection.commit()
+
+connection.close()
