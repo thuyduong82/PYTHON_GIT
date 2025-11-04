e89f268297365a729be596be032b490fac09b594
Thuy Duong <nguyen122@trebesin.cz>
2025-11-04 09:37:57 +0100

    seminarka

--- ZÁKLADY/10 SQLITE/database.py
+++ ZÁKLADY/10 SQLITE/database.py
@@ -6,15 +6,25 @@ import sqlite3
 connection = sqlite3.connect("game.db")
 
 #vytvoreni kurzoru, ktery nam umoznuje navigaco po databazi
-cursor = connection.cursor
+cursor = connection.cursor()
 
 #zapis do db
 
 user_input = input("přidej postavičku do databáze:")
+user_input2 = input("pridej class:")
 
-cursor.execute("INSERT INTO characters (name) VALUES (?)",((user_input,))
+cursor.execute("INSERT INTO characters (name,class) VALUES (?,?)",(user_input,user_input2))
+
+cursor.execute("SELECT * FROM characters")
+data = cursor.fetchall()
+
 
 #potvrzeni vlozeni dat do db
 connection.commit()
+
+cursor.execute("SELECT * FROM characters")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
 
 connection.close()
