dfaf47a58f9185228b9ccdd85382a18f07e6659c
Thuy Duong <nguyen122@trebesin.cz>
2026-01-09 14:02:37 +0100

    91

--- ZÁKLADY/pythonstiny.py
+++
@@ -1,8 +0,0 @@
-print(10 * "ovce")
-
-print("pes")#printne to na jiny radek
-print("pes")
-
-print("kocka", end=".")#end urcuje kde konci int neni to na dalsim radku
-print("kocka", end="\n")#\n je nový řádek a default viz pes
-print("kocka")
